e315cb5aba108b853a962d726890b91e817a7726
alandesenvolvedor <alanpn43@gmail.com>
2025-10-30 21:08:14 -0300

    metodo

--- 2-vue-metodo/src/App.vue
+++ 2-vue-metodo/src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld />
+  <div id="app">
+
+    <h1>{{ message }}</h1>
+
+    <p>Contador: {{ counter }}</p>
+    <button @click="incrementCounter">Aumentar</button>
+    <button @click="resetCounter">Zerar</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: 'Conteúdo da variavel mensagem!',
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++
+    },
+    resetCounter() {
+      this.counter = 0
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  margin: 5px;
+  padding: 8px 16px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #2a8f68;
 }
 </style>
