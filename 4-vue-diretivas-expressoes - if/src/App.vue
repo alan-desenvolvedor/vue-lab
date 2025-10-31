353c1aa938b0aa53bfcacf800e22ccec9d8efc49
alandesenvolvedor <alanpn43@gmail.com>
2025-10-31 20:40:01 -0300

    vue expressões diretivas

---
+++ 4-vue-diretivas-expressoes - if/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p v-if="estaVisivel">
+      ✨ MENSAGEM SECRETA REVELADA! ✨
+    </p>
+
+    <button @click="alternar">
+      {{ estaVisivel ? 'Esconder Mensagem' : 'Mostrar Mensagem' }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      estaVisivel: false 
+    }
+  },
+  methods: {
+
+    alternar() {
+      this.estaVisivel = !this.estaVisivel; 
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+button {
+  padding: 10px 20px;
+  cursor: pointer;
+}
+</style>
