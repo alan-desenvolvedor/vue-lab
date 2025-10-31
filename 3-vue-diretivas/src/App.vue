fa04e1929f33c3c28eab6ca69662680b672d53ee
alandesenvolvedor <alanpn43@gmail.com>
2025-10-30 22:04:28 -0300

    diretivas

---
+++ 3-vue-diretivas/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <input v-focus v-model="name" placeholder="Digite algo" />
+    <p v-if="name">Você digitou: {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  directives: {
+    focus: {
+      mounted(el) {
+        el.focus()
+      }
+    }
+  }
+}
+</script>
---
+++ 3-vue-diretivas/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{ msg }}</p>
+    <button @click="reverseMessage">Reverse Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+};
+</script>
