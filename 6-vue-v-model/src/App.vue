465ec544c1eb5854a1e032e9a0c8b08e33e0f1a1
alandesenvolvedor <alanpn43@gmail.com>
2025-10-31 21:29:55 -0300

    form submit

---
+++ 6-vue-v-model/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model.trim="username" />
+
+    <input type="checkbox" v-model="isChecked" />
+
+    <input v-model.lazy="email" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "john_doe",
+      isChecked: false,
+      email: "john.doe@example.com",
+    };
+  },
+};
+</script>
---
+++ 7-vue-evento-click/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <input v-model.trim="username" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: ""
+    };
+  },
+  methods: {
+    submitForm() {
+      alert(`Hello, ${this.username}`);
+    }
+  }
+};
+</script>
