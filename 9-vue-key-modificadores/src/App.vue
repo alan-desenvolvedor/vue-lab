9718a6131c84b57b15a8d5658e70a94e1d9d0974
alandesenvolvedor <alanpn43@gmail.com>
2025-10-31 21:36:52 -0300

    keys

---
+++ 9-vue-key-modificadores/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @keydown.enter="fazerAlgo" placeholder="Pressione Enter" />
+    <input @keydown.tab="tabPressionado" placeholder="Pressione Tab" />
+    <textarea @keydown.ctrl.enter="enviarFormulario" placeholder="Ctrl + Enter para Enviar"></textarea>
+    <input @keydown.enter.prevent="naoEnviar" placeholder="Enter não envia" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fazerAlgo() {
+      alert('Você pressionou Enter!');
+    },
+    tabPressionado() {
+      alert('Você pressionou Tab!');
+    },
+    enviarFormulario() {
+      alert('Formulário enviado com Ctrl + Enter');
+    },
+    naoEnviar() {
+      alert('Tecla Enter pressionada, mas a ação padrão foi impedida.');
+    }
+  }
+};
+</script>
